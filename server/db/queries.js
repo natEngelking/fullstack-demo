6e22793381b9dbca41a79a64606c63134b7bb5d9
natEngelking <natalie.mixdorf@gmail.com>
2020-04-22 11:57:36 -0600

    db and server up

--- server/db/queries.js
+++ server/db/queries.js
@@ -1,1 +1,31 @@
-// create db
+let mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/bugfullstack', {useNewUrlParser: true, useUnifiedTopology: true});
+
+let db = mongoose.connection;
+db.on('error', () => {
+  console.log('connection error');
+});
+  ;
+
+let bugSchema = new mongoose.Schema({
+  bugNumber: Number,
+  description: String,
+  reportedBy: String,
+  creationDate: String,
+  assignedTo: String,
+  threatLevel: String
+})
+
+let Bug = mongoose.model('Bug', bugSchema);
+
+let buggared = new Bug ({
+  bugNumber: 10,
+  description: 'awol',
+  reportedBy: 'me',
+  creationDate: 'today',
+  assignedTo: 'someone else',
+  threatLevel: 'probably pretty bad'
+})
+buggared.save();
+
+module.exports = Bug;
--- server/index.js
+++ server/index.js
@@ -1,1 +1,12 @@
 const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const port = 3000;
+//require database
+let Bug = require('./db/queries.js');
+//body parser--may need to install still
+
+
+app.listen(port, () => console.log('Listening to cool stuff at port 3000'));
+
+module.exports = app;
